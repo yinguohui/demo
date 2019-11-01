a1a0c6207dd14ece5e083e0ef20919add199d323
yinguohui <15196622412@163.com>
2019-11-01 13:28:59 +0800

    test

--- src/main/java/com/antiy/demo/DemoApplication.java
+++ src/main/java/com/antiy/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.antiy.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.antiy.demo.dao"})
 public class DemoApplication {
 
     public static void main(String[] args) {
