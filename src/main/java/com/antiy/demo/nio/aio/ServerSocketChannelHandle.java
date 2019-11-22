4962d689f92618cc60869917f48bbf6d053c475b
yinguohui <15196622412@163.com>
2019-11-22 17:33:46 +0800

    nio的一些研究

---
+++ src/main/java/com/antiy/demo/nio/aio/ServerSocketChannelHandle.java
@@ -0,0 +1,58 @@
+package com.antiy.demo.nio.aio;
+
+/**
+ * @Author ygh
+ * @Description
+ * @Date 2019/11/22
+ */
+
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
+/**
+ * 这个处理器类，专门用来响应 ServerSocketChannel 的事件。
+ * ServerSocketChannel只有一种事件：接受客户端的连接
+ * @author keep_trying
+ */
+public class ServerSocketChannelHandle implements CompletionHandler<AsynchronousSocketChannel, Void> {
+    /**
+     * 日志
+     */
+    private static final Log LOGGER = LogFactory.getLog(ServerSocketChannelHandle.class);
+
+    private AsynchronousServerSocketChannel serverSocketChannel;
+
+    /**
+     * @param serverSocketChannel
+     */
+    public ServerSocketChannelHandle(AsynchronousServerSocketChannel serverSocketChannel) {
+        this.serverSocketChannel = serverSocketChannel;
+    }
+
+    /**
+     * 注意，我们分别观察 this、socketChannel、attachment三个对象的id。
+     * 来观察不同客户端连接到达时，这三个对象的变化，以说明ServerSocketChannelHandle的监听模式
+     */
+    @Override
+    public void completed(AsynchronousSocketChannel socketChannel, Void attachment) {
+        ServerSocketChannelHandle.LOGGER.info("completed(AsynchronousSocketChannel result, ByteBuffer attachment)");
+        //每次都要重新注册监听（一次注册，一次响应），但是由于“文件状态标示符”是独享的，所以不需要担心有“漏掉的”事件
+        this.serverSocketChannel.accept(attachment, this);
+
+        //为这个新的socketChannel注册“read”事件，以便操作系统在收到数据并准备好后，主动通知应用程序
+        //在这里，由于我们要将这个客户端多次传输的数据累加起来一起处理，所以我们将一个stringbuffer对象作为一个“附件”依附在这个channel上
+        //
+        ByteBuffer readBuffer = ByteBuffer.allocate(2550);
+        //socketChannel.read(readBuffer, new StringBuffer(), new SocketChannelReadHandle(socketChannel , readBuffer));
+    }
+
+    @Override
+    public void failed(Throwable exc, Void attachment) {
+        ServerSocketChannelHandle.LOGGER.info("failed(Throwable exc, ByteBuffer attachment)");
+    }
+}
--- src/main/java/com/antiy/demo/nio/file/Test1.java
+++ src/main/java/com/antiy/demo/nio/file/Test1.java
@@ -1,9 +1,47 @@
 package com.antiy.demo.nio.file;
 
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
  * @Author ygh
  * @Description
  * @Date 2019/11/22
  */
 public class Test1 {
+    public static void main(String[] args) throws IOException {
+        // 获取文件来源 和 输出地址
+       // RandomAccessFile inputStream = new RandomAccessFile("D://1.txt","rw");
+       // RandomAccessFile outputStream = new RandomAccessFile("D://3.txt","rw");
+        FileInputStream inputStream = new FileInputStream("D://1.txt");
+        FileOutputStream outputStream = new FileOutputStream("D://2.txt");
+
+        // 获取文件通道
+        FileChannel fileChannel = inputStream.getChannel();
+        FileChannel outChannel = outputStream.getChannel();
+
+        // 建立缓存区
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        byteBuffer.put("你好".getBytes());
+        // 读取数据到缓存区(可能未读完，假设数据大于缓存区)
+        fileChannel.read(byteBuffer);
+
+        // 切换模式
+        byteBuffer.flip();
+
+        // 将缓存区数据输出到文件
+        outChannel.write(byteBuffer);
+
+
+        // 清理缓存区数据
+        byteBuffer.clear();
+
+        // 切换模式
+        byteBuffer.flip();
+
+        // 将缓存区数据输出到文件
+        outChannel.write(byteBuffer);
+
+    }
 }
