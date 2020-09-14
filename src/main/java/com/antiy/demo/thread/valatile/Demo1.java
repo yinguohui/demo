8357c6ea80a56417836c008bafe6f21e8af5de5a
yinguohui <15196622412@163.com>
2020-09-14 22:29:56 +0800

    jmm

---
+++ src/main/java/com/antiy/demo/thread/valatile/Demo1.java
@@ -0,0 +1,37 @@
+package com.antiy.demo.thread.valatile;
+
+/**
+ * @Author ygh
+ * @Description 测试
+ * @Date 2020/9/14
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        A a = new A();
+        a.start();
+
+        for (;  ; ) {
+            if (a.isFlag()){
+                System.out.println("有点东西");
+            }
+        }
+    }
+    static class A extends Thread{
+        private  boolean flag = false;
+
+        public boolean isFlag() {
+            return flag;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            flag = true;
+            System.out.println("flag" +flag);
+        }
+    }
+}
